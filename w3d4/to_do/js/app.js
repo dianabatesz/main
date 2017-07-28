e946fbb17fa857c47eaee3d9f0ac32616b63ab86
Diana Nicole Bates Zoullas <dianabatesz@Dianas-MacBook-Air.local>
2017-07-28 12:21:28 -0400

    7/28

--- w3d4/to_do/js/app.js
+++ w3d4/to_do/js/app.js
@@ -2,7 +2,12 @@ console.log('loaded');
 $(document).ready(function(){
 
 	$('#submit').click(function(event) {
-
-	});
-
-});
+		const $value = $("#input-box").val();
+        $("#to-do-list").append('<li>' + $value + '</li>');
+        });
+        $("to-do-list").on('click', 'li', function(x){
+        	const list1 = $(this).contents();
+        		console.log(list1);
+        });
+	
+}); 
